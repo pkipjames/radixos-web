d6dd18184f605e2037c48d6fa08e108614422cf5
James M <45425618+pkipjames@users.noreply.github.com>
2019-01-24 08:39:59 -0500

    Update appstarter.js

--- appstarter.js
+++ appstarter.js
@@ -29,16 +29,56 @@ document.getElementById(id).style.display="block";
 }catch(err){alert("ERROR\n\nshowScreen:\n"+err);}
 
 }
-
+function makeWindowsInactive(){
+var wins=document.getElementsByClassName("active");
+for( var i=0;i<wins.length;i++){
+  wins[i].classList.remove("active");
+}
+  
+  
+}
 function appOpen(parent,name,content,height,width,icon){
 var win=elt("div",{class:"appwindow"});
 if(icon){var ico=elt("img",{src:icon,height:"18px",alt:"icon"});}else{var ico=elt("span",{style:"border:1px solid #ffffff;"},(new String(name)).substring(0,1));}
 
-win.appendChild(elt("nav",{class:"titlebar"},icon,name));
+var nav1=elt("nav",{class:"titlebar",moving:"false"},ico,name);
+win.appendChild(nav1);
 
+win.appendChild(elt("div",{class:"content"},content));
+win.onclick=function (event){
+  if(!(win.classList.indexOf("active")!=-1)){makeWindowsInactive();win.classList.add("active");}
+};
+nav1.ontouchmove=function (event){
+  var x=event.pageX;
+  var y=event.pageY;
+  event.preventDefault();
+  win.style.top=y+"px";
+  win.style.left=x+"px";
+  win.style.height=height+"px";
+  win.style.width=width+"px";
+  
+  
+  
+  
+};
+  nav1.onmousedown=function (event){nav1.setAttribute("moving","true");};
+  nav1.onmouseup=function (event){nav1.setAttribute("moving","false");};
 
-
+  nav1.onmousemove=function (event){if(nav1.getAttribute("moving")=="true"){
+  var x=event.pageX;
+  var y=event.pageY;
+  
+  win.style.top=y+"px";
+  win.style.left=x+"px";
+  win.style.height=height+"px";
+  win.style.width=width+"px";
+  
+  
+  
+                                       }
+};
 
 parent.appendChild(win);
-return win
+  nav1.appendChild(elt("span",{onclick:function (){win.parentNode.removeChild(win);},style:"font-family:Ubuntu,'helvetica neue',verdana,sans-serif;font-weight:100;color:#ff0000;"},"x"));
+return win;
 }
