8e128a11164cf6945b9dcb5453528c94c56f7f03
James M <45425618+pkipjames@users.noreply.github.com>
2019-01-24 08:27:26 -0500

    Update appstarter.js

--- appstarter.js
+++ appstarter.js
@@ -14,6 +14,22 @@ function elt(name, attributes) {
   return node;
 }
 
+function showScreen(id){try{
+
+var screens=document.getElementsByClassName("screen");
+
+for (var i=0;i<screens.length;i++){
+
+screens[i].style.display="none";
+
+}
+
+document.getElementById(id).style.display="block";
+
+}catch(err){alert("ERROR\n\nshowScreen:\n"+err");}
+
+}
+
 function appOpen(parent,name,content,height,width,icon){
 var win=elt("div",{class:"appwindow"});
 if(icon){var ico=elt("img",{src:icon,height:"18px",alt:"icon"});}else{var ico=elt("span",{style:"border:1px solid #ffffff;"},(new String(name)).substring(0,1));}
